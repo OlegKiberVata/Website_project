db097d838d0204fd2152cbfa0f31c8ef4b673ace
OlegKiberVata <151632550+OlegKiberVata@users.noreply.github.com>
2024-03-26 04:31:37 +0700

    Update redact_form.py

--- root/forms/redact_form.py
+++ root/forms/redact_form.py
@@ -1,10 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, DateTimeField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Optional
 
 
 class RedactForm(FlaskForm):
-    name = StringField('Название', validators=[DataRequired()])
     info = StringField("Данные", validators=[DataRequired()])
-    date = DateTimeField('Дата напоминания', validators=[DataRequired()])
+    date = DateTimeField('Дата напоминания', validators=[Optional()])
     submit = SubmitField('Сохранить')
