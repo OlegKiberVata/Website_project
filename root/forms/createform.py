8a711eb6ab89e9f199939b1b11f2c1b5097a3d65
OlegKiberVata <151632550+OlegKiberVata@users.noreply.github.com>
2024-03-26 04:31:06 +0700

    Update createform.py

--- root/forms/createform.py
+++ root/forms/createform.py
@@ -1,10 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, DateTimeField, IntegerField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, DateTimeField
+from wtforms.validators import DataRequired, Optional
 
 
 class CreateForm(FlaskForm):
     name = StringField('Название', validators=[DataRequired()])
     info = StringField("Данные", validators=[DataRequired()])
-    date = DateTimeField('Дата напоминания', validators=[DataRequired()])
+    date = DateTimeField('Дата напоминания', validators=[Optional()])
     submit = SubmitField('Сохранить')
