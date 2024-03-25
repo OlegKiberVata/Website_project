2747b74c6070c372e0f40de622e555e35dc53223
OlegKiberVata <151632550+OlegKiberVata@users.noreply.github.com>
2024-03-26 00:35:17 +0700

    Update main.py

--- root/main.py
+++ root/main.py
@@ -13,13 +13,13 @@ person = None
 @app.route('/home/<person>')
 def home_page(person):
     global persons
-    return render_template('home.html', title='Главная страница', person=persons[person])
+    return render_template('home.html', title='Главная страница', person=person)
 
 
 @app.route("/profile/<person>")
 def profile(person):
     global persons
-    return render_template('profile.html', title='Профиль', profile=persons[person][3])
+    return render_template('profile.html', title='Профиль', profile=persons[person][3], person=person)
 
 
 @app.route("/note/<person>/<choosen_note>")
@@ -28,7 +28,7 @@ def note(person, choosen_note):
     return render_template('note.html', title="Просмотр записи", note=persons[person][2][choosen_note])
 
 
-@app.route("/note_creation/<person>")
+@app.route("/note_creation/<person>", methods=['GET', 'POST'])
 def create(person):
     global persons
     form = CreateForm()
@@ -39,7 +39,7 @@ def create(person):
     return render_template('create_note.html', title="Создание записи", person=person, note=note)
 
 
-@app.route("/profile_change/<person>")
+@app.route("/profile_change/<person>", methods=['GET', 'POST'])
 def change(person):
     global persons
     form = ChangeProfileForm()
@@ -47,11 +47,11 @@ def change(person):
         persons[person][3] = [form.name.data, form.surname.data, form.email.data,
                               form.password.data, form.gender.data, form.phone_number.data, form.age.data]
         # apply_data(persons[person]), передаю тебе штуки, чтобы занес в базу данных
-        return redirect(f'/home/{person}')
-    return render_template('change_prof.html', title="Смена данных пользователя", person=persons[person])
+        return redirect(f'/profile/{person}')
+    return render_template('change_prof.html', title="Смена данных пользователя", info=persons[person], person=person, form=form)
 
 
-@app.route("/redact_note/<person>/<note>")
+@app.route("/redact_note/<person>/<note>", methods=['GET', 'POST'])
 def redact(person, note):
     global persons
     form = RedactForm()
@@ -67,10 +67,10 @@ def registration():
     global persons
     form = RegistrForm()
     if form.validate_on_submit():  # and Check_registr([form.login.data, form.email.data, form.password.data]):, я передаю данные, ты проверяешь свободны ли они
-        persons[form.login.data] = [form.password.data, form.email.data, {}, []]  # Сохраняем данные пользователя в таком порядке: пароль, почта, записи, штуки для профиля
+        persons[form.login.data] = [form.password.data, form.email.data, {0: "Первая запись"}, ["Имя", "Фамилия", "Пол", "Возраст", "Почта", "Пароль", "Номер"]]  # Сохраняем данные пользователя в таком порядке: пароль, почта, записи, штуки для профиля
         # apply_data(persons[form.login.data]), передаю тебе штуки, чтобы занес в базу данных
         return redirect('/enter')
-    return render_template('registration.html', form=form, title='Регистрация')
+    return render_template('registration.html', form=form, title='Регистрация', person="None")
 
 
 @app.route('/enter', methods=['GET', 'POST'])  # вход
@@ -78,8 +78,8 @@ def login():
     global persons
     form = EnterForm()
     if form.validate_on_submit():  # and Check_enter([form.login.data, form.password.data]):, я передаю данные, ты проверяешь соотвествие и наличие их
-        return redirect(f'/home/{persons[form.login.data]}')
-    return render_template('enter.html', form=form, title='Вход')
+        return redirect(f'/profile/{form.login.data}')
+    return render_template('enter.html', form=form, title='Вход', person="None")
 
 
 if __name__ == '__main__':
